f14eaeb1eb4bfb0a9ab16dc7791bda643948a30d
MaratKhatipov <mmmarat86@gmail.com>
2021-07-04 13:32:23 +0500

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,11 @@
+package ru.job4j.generics;
+
+public class Animal {
+	private int weight;
+	private String habitat;
+
+	public Animal(int weight, String habitat) {
+		this.weight = weight;
+		this.habitat = habitat;
+	}
+}
