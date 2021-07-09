73e7556dc50e48c8ffb23f140d7b2fe9d2999901
MaratKhatipov <mmmarat86@gmail.com>
2021-07-09 17:08:45 +0500

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474 #175482] исправил

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,25 +2,13 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
 	private ForwardLinked<T> linked = new ForwardLinked<>();
-	private int size;
-/*
-Метод pop() - должен возвращать значение и удалять его из коллекции.
- */
+
 	public T pop() {
 		return linked.deleteFirst();
 	}
-/*
-Метод push(T value) - помещает значение в коллекцию.
- */
+
 	public void push(T value) {
 		linked.addFirst(value);
 	}
 }
 
-/*
-Примечание:
-При реализации стека на основе списка используются операции добавления и удаления
-с одного и того же конца. Например, можно сделать добавление в начало и удаление с начала.
-Либо с другого конца, т.е. с хвоста списка.
-В итоге мы получим тоже самое поведение.
- */
