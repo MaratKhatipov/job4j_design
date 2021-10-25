ec5d71f16299dd423c3e21139657c407c8ccc8dc
MaratKhatipov <mmmarat86@gmail.com>
2021-10-25 10:26:14 +0500

    6. Кодировка. [#862 #209099]

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+	public static void main(String[] args) {
+		boolean serverState = false;
+		try (ServerSocket server = new ServerSocket(9000)) {
+			System.out.println("Сервер запущен");
+			while (server.isClosed() == serverState) {
+				Socket socket = server.accept();
+				try (OutputStream out = socket.getOutputStream();
+					 BufferedReader in = new BufferedReader(
+							 new InputStreamReader(socket.getInputStream()))) {
+					out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+					for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+						System.out.println(str);
+						if (str.contains("Bye")) {
+							out.write("Работа сервера завершена".getBytes());
+							System.out.println("Сервер остановлен");
+							serverState = true;
+						}
+					}
+					out.flush();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
