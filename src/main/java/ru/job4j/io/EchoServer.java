3b03157756adafc8758a598df54d88d6707e2290
MaratKhatipov <mmmarat86@gmail.com>
2021-11-16 16:46:06 +0500

    0. Что такое Socket? [#4850 #220277] fixed encoding

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class EchoServer {
 	public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class EchoServer {
 					for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
 						System.out.println(str);
 						if (str.contains("Bye")) {
-							out.write("Работа сервера завершена".getBytes());
+							out.write("Работа сервера завершена".getBytes(Charset.forName("WINDOWS-1251")));
 							System.out.println("Сервер остановлен");
 							serverState = true;
 
