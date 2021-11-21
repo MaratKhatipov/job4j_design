3b08a2ec29eab3cdc4d3535f061ca6fce16a3b9e
MaratKhatipov <mmmarat86@gmail.com>
2021-11-21 14:48:44 +0500

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/NestedObject.java
+++ src/main/java/ru/job4j/serialization/json/NestedObject.java
@@ -1,8 +1,18 @@
 package ru.job4j.serialization.json;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "nested_object")
 public class NestedObject {
+
+	@XmlElement
 	private String color;
+	@XmlElement
 	private int volume;
+
+	public NestedObject() {
+	}
 
 	public NestedObject(String color, int volume) {
 		this.color = color;
