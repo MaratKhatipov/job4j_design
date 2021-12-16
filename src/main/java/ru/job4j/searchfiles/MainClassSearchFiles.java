639818c366c82392ad523b82d205a281bf93b616
MaratKhatipov <mmmarat86@gmail.com>
2021-12-16 16:04:11 +0500

    2. Поиск файлов по критерию [#783 #232222] ешё раз

--- src/main/java/ru/job4j/searchfiles/MainClassSearchFiles.java
+++ src/main/java/ru/job4j/searchfiles/MainClassSearchFiles.java
@@ -1,10 +1,16 @@
 package ru.job4j.searchfiles;
 
 
-
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class MainClassSearchFiles {
@@ -22,23 +28,27 @@ public class MainClassSearchFiles {
 		this.typeSearch = typeSearch;
 	}
 
-	public void search() {
+	public static Predicate<Path> predicate(String searchFile, String typeSearch) {
+		Predicate<Path> predictor = null;
 		Pattern pattern;
 		try {
-			Path start = Paths.get(String.valueOf(directory));
 			if ("name".equals(typeSearch)) {
-				list = Search.search(start, p -> p.toFile().getName().equals(searchFile));
+				String finalSearchFile = searchFile;
+				predictor = p -> p.toFile().getName().equals(finalSearchFile);
 			} else if ("mask".equals(typeSearch)) {
-				searchFile = searchMask();
-				pattern = Pattern.compile(searchFile);
-				list = Search.search(start, p -> p.toFile().getName().matches(pattern.toString()));
+				String mask = searchFile.replaceAll("\\*", ".\\*");
+				mask = mask.replaceAll("\\?", ".\\?");
+				pattern = Pattern.compile(mask);
+				predictor = p -> p.toFile().getName().matches(pattern.toString());
 			} else if ("regex".equals(typeSearch)) {
 				pattern = Pattern.compile(searchFile);
-				list = Search.search(start, p -> pattern.matcher(p.toFile().getName()).find());
+				predictor = p -> pattern.matcher(p.toFile().getName()).find();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		return predictor;
 	}
 
 	private String searchMask() {
@@ -47,25 +57,37 @@ public class MainClassSearchFiles {
 		return mask;
 	}
 
-	public void save() {
-		search();
+	public static  void save(Path target, List<Path> pathList) {
 		try (PrintWriter out = new PrintWriter(
 				new BufferedOutputStream(
-						new FileOutputStream(outFile, true)))) {
-			list.stream().map(p -> p.getFileName().toString()).forEach(out::println);
+						new FileOutputStream(target.toFile())))) {
+			for (Path path :pathList) {
+				out.println(path.toString());
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void fileSearch(ArgumentsName argumentsName) throws IOException {
+	Path root = Paths.get(argumentsName.get("d"));
+	Path target = Paths.get(argumentsName.get("o"));
+	String searchName = argumentsName.get("n");
+	String typeSearch = argumentsName.get("t");
 
+	SearchFiles searchFiles = new SearchFiles(predicate(searchName, typeSearch));
+	Files.walkFileTree(root, searchFiles);
+	save(target, searchFiles.searchList);
+	}
+
+
+	public static void main(String[] args) throws IOException {
 		Validate validate = new Validate(args);
-		MainClassSearchFiles searchFiles = new MainClassSearchFiles(
-				validate.getDirectory(), validate.getSearchFile(),
-				validate.getOutFile(), validate.getTypeSearch()
-		);
-		searchFiles.save();
+
+		ArgumentsName argName = ArgumentsName.of(args);
+
+		fileSearch(argName);
 		System.out.println("_______");
 	}
 }
