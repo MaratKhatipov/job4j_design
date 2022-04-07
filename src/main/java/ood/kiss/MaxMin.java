b2ff787c09b0df1d53cfa07432f926f4b4c610cc
MaratKhatipov <mmmarat86@gmail.com>
2022-04-07 19:23:35 +0500

    1. Принципы Kiss, Dry и Yagni [#238813] v0.2

--- src/main/java/ood/kiss/MaxMin.java
+++ src/main/java/ood/kiss/MaxMin.java
@@ -1,4 +1,4 @@
-package kiss;
+package ood.kiss;
 
 import java.util.Comparator;
 import java.util.List;
--- src/test/java/kiss/MaxMinTest.java
+++ src/test/java/kiss/MaxMinTest.java
@@ -1,5 +1,6 @@
 package kiss;
 
+import ood.kiss.MaxMin;
 import org.junit.Test;
 
 import java.util.ArrayList;
