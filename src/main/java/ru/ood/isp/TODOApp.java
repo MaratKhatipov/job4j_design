1ec555e95d72248f316f3d746ae17de06ba25a21
MaratKhatipov <mmmarat86@gmail.com>
2022-06-02 11:32:19 +0500

    1. Создать меню. [#4748] v1.1

--- src/main/java/ru/ood/isp/TODOApp.java
+++ src/main/java/ru/ood/isp/TODOApp.java
@@ -6,29 +6,27 @@ import java.util.StringJoiner;
 public class TODOApp {
 
     public static final ActionDelegate STUB_ACTION = () -> System.out.println("меню выбрано");
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private static  Scanner scanner = new Scanner(System.in);
 
-    private static boolean init(String request, Menu menu, MenuPrinter printer) {
+    private static boolean init(Menu menu, String request, MenuPrinter printer) {
         boolean result = true;
-        switch (request) {
-            case "1" -> {
-                System.out.println("Enter a name for the new task/Введите имя новой задачи: ");
-                menu.add(Menu.ROOT, SCANNER.nextLine(), STUB_ACTION);
-            }
-            case "2" -> {
-                printer.print(menu);
-                System.out.println("Choose a task name/Выберите имя задачи:");
-                String parent = SCANNER.nextLine();
-                System.out.println("Enter a subtask name/Введите имя подзадачи:");
-                String child = SCANNER.nextLine();
-                menu.add(parent, child, STUB_ACTION);
-            }
-            case "3" -> printer.print(menu);
-            case "4" -> {
-                System.out.println("GG WP");
-                result = false;
-            }
-            default -> System.out.println("Введите цифры от 1 до 4");
+        if ("1".equals(request)) {
+            System.out.println("Enter a name for the new task/Введите имя новой задачи: ");
+            menu.add(Menu.ROOT, scanner.nextLine(), STUB_ACTION);
+        } else if ("2".equals(request)) {
+            printer.print(menu);
+            System.out.println("Choose a task name/Выберите имя задачи:");
+            String parent = scanner.nextLine();
+            System.out.println("Enter a subtask name/Введите имя подзадачи:");
+            String child = scanner.nextLine();
+            menu.add(parent, child, STUB_ACTION);
+        } else if ("3".equals(request)) {
+            printer.print(menu);
+        } else if ("4".equals(request)) {
+            System.out.println("GG WP");
+            result = false;
+        } else {
+            System.out.println("Введите цифры от 1 до 4");
         }
         return result;
     }
@@ -49,7 +47,7 @@ public class TODOApp {
         MenuPrinter printer = new Printer();
         while (run) {
             menuCreate();
-            run = init(SCANNER.nextLine(), menuEx, printer);
+            run = init(menuEx, scanner.nextLine(), printer);
         }
     }
 }
